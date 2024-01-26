e8f4e12a0e0260af8b16d6ccc0831bbd3c890c3f
Yogann <yogann.henry@perfusion.dev>
2024-01-26 15:00:30 +0100

    creation branch develop

--- src/views/dashboard.vue
+++ src/views/dashboard.vue
@@ -3,7 +3,7 @@
 
 <template>
   <section class="block shadow flex-center" style="width: 100%; min-height: 100%; color: var(--gray); font-size: 5rem;">
-    To be continued
+    Salut Michel
   </section>
 </template>
 
